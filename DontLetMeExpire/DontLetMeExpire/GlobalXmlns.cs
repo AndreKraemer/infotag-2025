4ad58f47573188e049acca28401f42903c04663b
Andre Kraemer <andre@andrekraemer.de>
2025-06-23 19:59:24 +0200

    GlobalXmlns und ImplicitXmlNs

---
+++ DontLetMeExpire/DontLetMeExpire/GlobalXmlns.cs
@@ -0,0 +1,28 @@
+﻿[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "DontLetMeExpire")]
+
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "DontLetMeExpire.Controls")]
+
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "DontLetMeExpire.Models")]
+
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "DontLetMeExpire.Resources.Strings")]
+
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "DontLetMeExpire.ViewModels")]
+
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "http://schemas.syncfusion.com/maui/toolkit")]
+
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "http://schemas.microsoft.com/dotnet/2022/maui/toolkit")]
+
